15f9e8be3fb3312b360310db52a1247240e06168
DELFINA MONCAYO <mon19012@byui.edu>
2021-05-14 18:02:22 -0400

    yolo

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-     `${MONGODBURL}`
+     `${MONGODBURL}`, { useNewUrlParser: true }
   )
   .then(result => {
     app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
